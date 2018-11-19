6fce684bd459e685c29a18b25fbffbb6bbed05ed
Av <vonnetworking@gmail.com>
2018-11-18 20:30:04 -0500

    updated apply_manifest script to skip items using do_not_auto_apply.list

--- apply_manifests.sh
+++ apply_manifests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+# starts the pods defined in the current directory as long as they are not
+# listed in the "do_not_auto_apply.list"
 
-for F in $(ls -1 ./*.yml); do
+for F in $(ls -1 ./*.yml | grep -v -f ./do_not_auto_apply.list); do
   kubectl apply -f ${F}
 done
